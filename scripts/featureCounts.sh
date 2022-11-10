dbe5250bc19ce2c098251d12546828633fffe686
Anita Salamon <62751614+anitasalamon@users.noreply.github.com>
2022-11-10 14:48:37 -0500

    Create featureCounts.sh

---
+++ scripts/featureCounts.sh
@@ -0,0 +1,5 @@
+cd /home/as3gd/bin/subread-1.6.2-source/bin
+./featureCounts -a /project/genomes/Mus_musculus/NCBI/GRCm38/Annotation/Genes/genes.gtf -o /project/Owens_Ri
+vanna/00.Raw.and.Aligned.Files/bulk-RNA-seq/2022.09.AS_VS_M001/usftp21.novogene.com/03_align/AS_VS_M001.counts2.txt -p
+ -B -T 12 -g gene_id /project/Owens_Rivanna/00.Raw.and.Aligned.Files/bulk-RNA-seq/2022.09.AS_VS_M001/usftp21.novogene.
+com/03_align/*.sortedByCoord.out.bam
